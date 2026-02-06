71da988aaa9d71db5d965a98f1335d119b0faa16
Alvaro <alvaroaxelle1008@gmail.com>
2026-02-06 09:42:02 +0700

    Menambahkan Array List pada status dan Memunculkan Data pada Firebase

--- app/src/main/java/com/alvaro/projectpenjualan/MainActivity.kt
+++ app/src/main/java/com/alvaro/projectpenjualan/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.alvaro.projectpenjualan
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.alvaro.projectpenjualan.kategori.DataKategori
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var CardKategori : CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        init()
+
+        CardKategori.setOnClickListener {
+            val intent = Intent(this@MainActivity, DataKategori::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+    }
+
+    fun init() {
+        CardKategori = findViewById(R.id.cv4)
     }
 }
--- app/src/main/java/com/alvaro/projectpenjualan/kategori/DataKategori.kt
+++ app/src/main/java/com/alvaro/projectpenjualan/kategori/DataKategori.kt
@@ -1,21 +1,38 @@
-package kategori
+package com.alvaro.projectpenjualan.kategori
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.alvaro.projectpenjualan.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class ModKategori : AppCompatActivity() {
+class DataKategori : AppCompatActivity() {
+    private lateinit var fabData: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_mod_kategori)
+        setContentView(R.layout.activity_data_kategori)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        init()
+
+        fabData = findViewById(R.id.fabDATA_KATEGORI_Tambah)
+        fabData.setOnClickListener {
+            val intent = Intent(this, ModKategori::class.java)
+            startActivity(intent)
+        }
+    }
+    fun init(){
+        fabData = findViewById(R.id.fabDATA_KATEGORI_Tambah)
+
     }
 }
+
